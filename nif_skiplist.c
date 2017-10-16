33dd0c574d0a7b59cc8e54303cf23b300ff9bcb7
juliusfox <jinglingxiaokui@gmail.com>
2017-10-16 14:50:55 +0800

    makefile

--- nif_skiplist.c
+++ nif_skiplist.c
@@ -353,6 +353,6 @@ static ErlNifFunc nif_funcs[] = {
         {"range_by_score", 3, range_by_score},
 };
 
-ERL_NIF_INIT(anif, nif_funcs, NULL, NULL, NULL, NULL)
+ERL_NIF_INIT(skiplist, nif_funcs, NULL, NULL, NULL, NULL)
 
 
